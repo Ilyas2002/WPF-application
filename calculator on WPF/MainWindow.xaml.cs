a4392702dcfa7a5435e5d1f7a57b24266cb0bfd0
Ilyas2002 <57584228+Ilyas2002@users.noreply.github.com>
2021-01-26 19:54:34 +0500

    Add files via upload

---
+++ calculator on WPF/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace custom_application
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            foreach(UIElement el in grid.Children)
+            {
+                if(el is Button)
+                {
+                    ((Button)el).Click += Button_Click;
+                }
+            }
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string str = (string)((Button)e.OriginalSource).Content;
+
+            if (str == "AC")
+                textLabel1.Text = "";
+            else if(str == "=")
+            {
+                string viue = new DataTable().Compute(textLabel1.Text, null).ToString();
+                textLabel1.Text= viue;
+            }
+            else
+                textLabel1.Text += str;
+
+
+        }
+    }
+}
